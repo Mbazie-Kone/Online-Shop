acb33574dc0e0a894c5c669182fce0836dc20df8
M'bazie Kone <mbazie89@gmail.com>
2024-05-25 23:32:05 +0200

    Firs commit

---
+++ src/test/java/it/mbaziekone/oline_shop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package it.mbaziekone.oline_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
