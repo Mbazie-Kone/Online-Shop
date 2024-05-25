8dc52095bf3bce2b45851c8300665546e528b363
M'bazie Kone <mbazie89@gmail.com>
2024-05-25 23:42:08 +0200

    Updates

--- src/main/java/it/mbaziekone/oline_shop/model/Product.java
+++ src/main/java/it/mbaziekone/oline_shop/model/Product.java
@@ -1,6 +1,11 @@
 package it.mbaziekone.oline_shop.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "Products")
 public class Product {
-
+	
+	
 }
