f402b4f569213431529384aadd1f3393356a054b
M'bazie Kone <mbazie89@gmail.com>
2024-05-27 22:38:09 +0200

    Updates

--- src/main/java/it/mbaziekone/oline_shop/controller/CategoryController.java
+++ src/main/java/it/mbaziekone/oline_shop/controller/CategoryController.java
@@ -3,11 +3,13 @@ package it.mbaziekone.oline_shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import it.mbaziekone.oline_shop.service.CategoryService;
 
 @Controller
+@RequestMapping("category")
 public class CategoryController {
 	
 	@Autowired
--- src/main/java/it/mbaziekone/oline_shop/controller/ProductController.java
+++ src/main/java/it/mbaziekone/oline_shop/controller/ProductController.java
@@ -3,11 +3,13 @@ package it.mbaziekone.oline_shop.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import it.mbaziekone.oline_shop.service.ProductService;
 
 @Controller
+@RequestMapping("product")
 public class ProductController {
 	
 	@Autowired
