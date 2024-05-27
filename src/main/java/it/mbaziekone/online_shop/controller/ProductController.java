d1c7bc988cbc511648a136aadfee2ba4de9d2cff
M'bazie Kone <mbazie89@gmail.com>
2024-05-27 23:27:37 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/controller/ProductController.java
+++ src/main/java/it/mbaziekone/online_shop/controller/ProductController.java
@@ -1,11 +1,14 @@
 package it.mbaziekone.online_shop.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import it.mbaziekone.online_shop.model.Product;
 import it.mbaziekone.online_shop.service.ProductService;
 
 @Controller
@@ -18,7 +21,12 @@ public class ProductController {
 	@GetMapping("/")
 	@ResponseBody
 	public String index() {
-		productService.findAll();
-		return "Product page";
+		return "Product Page";
+	}
+	
+	@GetMapping("/all")
+	public List<Product> products() {
+		List<Product> list = productService.findAll();
+		return list;
 	}
 }
--- src/main/java/it/mbaziekone/online_shop/service/CategoryService.java
+++ src/main/java/it/mbaziekone/online_shop/service/CategoryService.java
@@ -2,7 +2,6 @@ package it.mbaziekone.online_shop.service;
 
 import java.util.List;
 
-
 import it.mbaziekone.online_shop.model.Category;
 
 public interface CategoryService {
