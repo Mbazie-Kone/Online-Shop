4ed359c1f82157796dc31c15ce09068c750a3857
M'bazie Kone <mbazie89@gmail.com>
2024-05-27 20:22:59 +0200

    Updates

--- src/main/java/it/mbaziekone/oline_shop/model/Product.java
+++ src/main/java/it/mbaziekone/oline_shop/model/Product.java
@@ -1,12 +1,17 @@
 package it.mbaziekone.oline_shop.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Products")
 public class Product {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	private String name;
@@ -16,4 +21,6 @@ public class Product {
 	private double price;
 	
 	private Category category;
+	
+	private String imageUrl;
 }
