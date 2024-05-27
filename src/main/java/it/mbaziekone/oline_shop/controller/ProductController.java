dd92eb56cb8de4107adda3fe99db1e65f931442b
M'bazie Kone <mbazie89@gmail.com>
2024-05-27 22:47:14 +0200

    Updates

--- src/main/java/it/mbaziekone/oline_shop/controller/ProductController.java
+++ src/main/java/it/mbaziekone/oline_shop/controller/ProductController.java
@@ -1,4 +1,4 @@
-package it.mbaziekone.oline_shop.controller;
+package it.mbaziekone.online_shop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import it.mbaziekone.oline_shop.service.ProductService;
+import it.mbaziekone.online_shop.service.ProductService;
 
 @Controller
 @RequestMapping("product")
--- src/main/java/it/mbaziekone/online_shop/OnlineShopApplication.java
+++ src/main/java/it/mbaziekone/online_shop/OnlineShopApplication.java
@@ -1,4 +1,4 @@
-package it.mbaziekone.oline_shop;
+package it.mbaziekone.online_shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
