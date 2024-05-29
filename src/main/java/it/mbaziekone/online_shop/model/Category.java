632c24c15d16ccbc4edfd4ff016c12b1bf8952a1
M'bazie Kone <mbazie89@gmail.com>
2024-05-30 01:42:44 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/Category.java
+++ src/main/java/it/mbaziekone/online_shop/model/Category.java
@@ -1,10 +1,14 @@
 package it.mbaziekone.online_shop.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,5 +21,8 @@ public class Category {
 	private Long id;
 	
 	private String name;
+	
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Product> products;
 
 }
