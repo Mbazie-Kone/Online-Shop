7a569ca79218c220dc8ef2cfd08404f3914bb9f4
M'bazie Kone <mbazie89@gmail.com>
2024-05-30 01:49:56 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/Product.java
+++ src/main/java/it/mbaziekone/online_shop/model/Product.java
@@ -1,5 +1,7 @@
 package it.mbaziekone.online_shop.model;
 
+import java.util.Objects;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -40,5 +42,113 @@ public class Product {
 	@Column(name = "review_count")
 	private int reviewCount;
 
+	public Product() {}
 
+	public Product(Long id, String name, String description, double price, int stockQuantity, Category category,
+			String imageUrl, double averageRating, int reviewCount) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.stockQuantity = stockQuantity;
+		this.category = category;
+		this.imageUrl = imageUrl;
+		this.averageRating = averageRating;
+		this.reviewCount = reviewCount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public double getAverageRating() {
+		return averageRating;
+	}
+
+	public void setAverageRating(double averageRating) {
+		this.averageRating = averageRating;
+	}
+
+	public int getReviewCount() {
+		return reviewCount;
+	}
+
+	public void setReviewCount(int reviewCount) {
+		this.reviewCount = reviewCount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(averageRating, category, description, id, imageUrl, name, price, reviewCount,
+				stockQuantity);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Double.doubleToLongBits(averageRating) == Double.doubleToLongBits(other.averageRating)
+				&& Objects.equals(category, other.category) && Objects.equals(description, other.description)
+				&& Objects.equals(id, other.id) && Objects.equals(imageUrl, other.imageUrl)
+				&& Objects.equals(name, other.name)
+				&& Double.doubleToLongBits(price) == Double.doubleToLongBits(other.price)
+				&& reviewCount == other.reviewCount && stockQuantity == other.stockQuantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + ", description=" + description + ", price=" + price
+				+ ", stockQuantity=" + stockQuantity + ", category=" + category + ", imageUrl=" + imageUrl
+				+ ", averageRating=" + averageRating + ", reviewCount=" + reviewCount + "]";
+	}
 }
