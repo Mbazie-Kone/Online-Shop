8863d398f870e7496e278092756effdb002f02f9
M'bazie Kone <mbazie89@gmail.com>
2024-05-29 22:19:29 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/Product.java
+++ src/main/java/it/mbaziekone/online_shop/model/Product.java
@@ -27,43 +27,5 @@ public class Product {
 	
 	private String sku;
 
-	public String getName() {
-		return name;
-	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public double getPrice() {
-		return price;
-	}
-
-	public void setPrice(double price) {
-		this.price = price;
-	}
-
-	public String getImageUrl() {
-		return imageUrl;
-	}
-
-	public void setImageUrl(String imageUrl) {
-		this.imageUrl = imageUrl;
-	}
-
-	public String getSku() {
-		return sku;
-	}
-
-	public void setSku(String sku) {
-		this.sku = sku;
-	}
 }
