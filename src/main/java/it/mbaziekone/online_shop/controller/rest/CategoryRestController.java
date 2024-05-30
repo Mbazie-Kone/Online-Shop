6d8ad0954077afa65c9ea904530b1af38dd59515
M'bazie Kone <mbazie89@gmail.com>
2024-05-30 02:15:35 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/controller/rest/CategoryRestController.java
+++ src/main/java/it/mbaziekone/online_shop/controller/rest/CategoryRestController.java
@@ -3,12 +3,14 @@ package it.mbaziekone.online_shop.controller.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.mbaziekone.online_shop.model.Category;
 import it.mbaziekone.online_shop.service.CategoryService;
 
 @RestController
+@RequestMapping("api/products")
 public class CategoryRestController {
 	
 	@Autowired
