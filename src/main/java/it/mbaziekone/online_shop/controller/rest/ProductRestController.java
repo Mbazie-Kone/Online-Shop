424f7f1a9f2502c75f4d0748393d988679fdd9d4
M'bazie Kone <mbazie89@gmail.com>
2024-05-30 02:21:52 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/controller/rest/ProductRestController.java
+++ src/main/java/it/mbaziekone/online_shop/controller/rest/ProductRestController.java
@@ -3,6 +3,7 @@ package it.mbaziekone.online_shop.controller.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class ProductRestController {
 	@Autowired
 	private ProductService productService;
 	
+	@GetMapping
 	public List<Product> products() {
 		
 		List<Product> products = productService.findAllProducts();
