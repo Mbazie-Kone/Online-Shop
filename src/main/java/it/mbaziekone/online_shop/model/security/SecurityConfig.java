27c7463f4e7a39f3bc2e4417054129a6bab18474
M'bazie Kone <mbazie89@gmail.com>
2024-06-01 00:06:43 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
+++ src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
@@ -1,38 +1,17 @@
 package it.mbaziekone.online_shop.model.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import jakarta.servlet.Filter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
-	@Autowired
-	private UserDetailsService userDetailsService;
-	
 	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
 	}
-	
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
-	}
-
-	@Override
-	public Filter springSecurityFilterChain() throws Exception {
-		// TODO Auto-generated method stub
-		return super.springSecurityFilterChain();
-	}
-	
-	
 }
