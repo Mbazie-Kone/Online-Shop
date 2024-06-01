6ed6328551b4177f3cdc0e048c425e6d249cb12f
M'bazie Kone <mbazie89@gmail.com>
2024-06-01 08:45:37 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
+++ src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
@@ -2,7 +2,6 @@ package it.mbaziekone.online_shop.model.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,11 +12,12 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-		http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/admin/**").hasRole("ADMIN")
-				.requestMatchers("/user/**").hasRole("USER").anyRequest().authenticated())
-				.formLogin(formLogin -> formLogin.loginPage("/login").permitAll())
-				.rememberMe(Customizer.withDefaults());
+		
+		http.authorizeHttpRequests()
+		.requestMatchers("/admin/**").hasRole("ADMIN")
+		.requestMatchers("/user/**").hasRole("USER")
+		.requestMatchers("/login").permitAll().anyRequest().authenticated()
+		.and().formLogin().loginPage("/login").permitAll().and().rememberMe();
 
 		return http.build();
 
