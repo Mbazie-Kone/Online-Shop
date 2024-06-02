8ae4ca5d36ceec046edc58323271d6b510671c39
M'bazie Kone <mbazie89@gmail.com>
2024-06-02 06:20:33 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/service/impl/CustomUserDetailsService.java
+++ src/main/java/it/mbaziekone/online_shop/service/impl/CustomUserDetailsService.java
@@ -1,10 +1,16 @@
 package it.mbaziekone.online_shop.service.impl;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import it.mbaziekone.online_shop.model.security.User;
 import it.mbaziekone.online_shop.repository.UserRepository;
 
 public class CustomUserDetailsService implements UserDetailsService {
@@ -14,8 +20,16 @@ public class CustomUserDetailsService implements UserDetailsService {
 	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthorities(user));
 	}
-
+	
+	private Collection<? extends GrantedAuthority> getAuthorities(User user) {
+		
+		return List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+	}
 }
