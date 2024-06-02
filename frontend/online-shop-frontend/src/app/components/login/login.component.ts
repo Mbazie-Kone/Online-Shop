cb6844761f9eb9999bb0c60290a915d3ba4b03e1
M'bazie Kone <mbazie89@gmail.com>
2024-06-02 10:35:00 +0200

    Updates

--- frontend/online-shop-frontend/src/app/components/login/login.component.ts
+++ frontend/online-shop-frontend/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string;
+  password: string;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.authService.login(this.username, this.password).subscribe(data => {
+      localStorage.setItem('token', data.token);
+      if(data.role === 'ADMIN') {
+        this.router.navigate(['/admin']);
+      }
+      else if(data.role === 'USER') {
+        this.router.navigate(['/user'])
+      }
+    })
+  }
 }
