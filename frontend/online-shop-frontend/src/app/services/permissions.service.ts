02017999a8ecad9c19b28ea99a9a2b565f5cb0ea
M'bazie Kone <mbazie89@gmail.com>
2024-06-02 10:02:34 +0200

    Updates

--- frontend/online-shop-frontend/src/app/services/permissions.service.ts
+++ frontend/online-shop-frontend/src/app/services/permissions.service.ts
@@ -1,15 +1,27 @@
 import { Injectable, inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PermissionsService {
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActive(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
 
+    const currentUser = this.authService.isLoggedIn();
+    if(currentUser) {
+      const roles = route.data['roles'] as Array<string>;
+      if(roles && roles.indexOf(currentUser.role) === -1) {
+        this.router.navigate(['/login']);
+        return false;
+      }
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
   }
 }
 
