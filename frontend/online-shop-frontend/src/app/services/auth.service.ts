c430e3308ab74e4788110c2e4eee10a9beaeb29c
M'bazie Kone <mbazie89@gmail.com>
2024-06-02 09:28:50 +0200

    Updates

--- frontend/online-shop-frontend/src/app/services/auth.service.ts
+++ frontend/online-shop-frontend/src/app/services/auth.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +9,13 @@ export class AuthService {
 
   private apiUrl = 'http://localhost:8080';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, {username, password});
+  }
+
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('token');
+  }
 }
