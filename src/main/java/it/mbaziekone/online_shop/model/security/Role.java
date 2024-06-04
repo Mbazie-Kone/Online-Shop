dea3ddd47f6685530c0b4d2c58989f9ae37ee0ba
M'bazie Kone <mbazie89@gmail.com>
2024-06-05 01:47:48 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/online_shop/model/security/Role.java
@@ -0,0 +1,23 @@
+package it.mbaziekone.online_shop.model.security;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "role_id")
+	private Long id;
+
+}
