ff1b59cdf1e3cd620514c8e25c64a2e59f2061d8
M'bazie Kone <mbazie89@gmail.com>
2024-06-11 01:02:57 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/security/User.java
+++ src/main/java/it/mbaziekone/online_shop/model/security/User.java
@@ -1,19 +1,12 @@
 package it.mbaziekone.online_shop.model.security;
 
 import java.io.Serializable;
-import java.util.Set;
-
-import javax.management.relation.Role;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,9 +24,7 @@ public class User implements Serializable {
 	
 	private String password;
 	
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn (name = "role_id"))
-	private Set<Role> roles;
+	private String role;
 
 	public String getUsername() {
 		return username;
@@ -51,12 +42,12 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
-	public Set<Role> getRoles() {
-		return roles;
+	public String getRole() {
+		return role;
 	}
 
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
+	public void setRole(String role) {
+		this.role = role;
 	}
-	
+
 }
