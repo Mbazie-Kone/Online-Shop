4ca3ad6f3ad2653a55d365bef0202402ae6be0bb
M'bazie Kone <mbazie89@gmail.com>
2024-06-11 01:54:58 +0200

    Updates

--- frontend/online-shop-frontend/src/app/app.module.ts
+++ frontend/online-shop-frontend/src/app/app.module.ts
@@ -3,26 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
-import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
 import { UserComponent } from './components/user/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    AdminDashboardComponent,
-    UserDashboardComponent,
-    UserComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
