e97155fdddf6b9d22547f99ec437add682e81876
M'bazie Kone <mbazie89@gmail.com>
2024-06-11 02:11:04 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/controller/rest/UserRestController.java
+++ src/main/java/it/mbaziekone/online_shop/controller/rest/UserRestController.java
@@ -3,7 +3,6 @@ package it.mbaziekone.online_shop.controller.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
