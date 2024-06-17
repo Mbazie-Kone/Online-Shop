1c92a65c257fa21f0de8c4c34e4894dcf5f67b55
M'bazie Kone <mbazie89@gmail.com>
2024-06-17 23:43:14 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/config/WebConfig.java
+++ src/main/java/it/mbaziekone/online_shop/config/WebConfig.java
@@ -1,13 +1,6 @@
 package it.mbaziekone.online_shop.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 	
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedOrigins("http://localhost:4200");
-	}
 }
