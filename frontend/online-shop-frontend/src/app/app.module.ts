1aa8de0cc8e5786b5c7ac754c005f1a680a481eb
M'bazie Kone <mbazie89@gmail.com>
2024-06-17 23:35:16 +0200

    Updates

--- frontend/online-shop-frontend/src/app/app.module.ts
+++ frontend/online-shop-frontend/src/app/app.module.ts
@@ -6,14 +6,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { UserFormComponent } from './components/user/user-form/user-form.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserFormComponent
   ],
   imports: [
     BrowserModule,
