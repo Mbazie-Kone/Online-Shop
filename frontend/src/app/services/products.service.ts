13e4219e809ad8a7ec18eab69113485d6bff5320
M'bazie Kone <mbazie89@gmail.com>
2024-06-21 01:27:55 +0200

    Updates

--- frontend/src/app/services/products.service.ts
+++ frontend/src/app/services/products.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private baseUrl = 'http://localhost:8080/api/products'
+  private baseUrl = 'http://localhost:8080/api/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+
 }
