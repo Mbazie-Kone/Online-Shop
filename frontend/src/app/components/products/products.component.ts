76ae8ac76cc08bb43e8cb214774b291fef4eac54
M'bazie Kone <mbazie89@gmail.com>
2024-06-21 01:33:02 +0200

    Updates

--- frontend/src/app/components/products/products.component.ts
+++ frontend/src/app/components/products/products.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+
+  products: any[] = [];
+
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
