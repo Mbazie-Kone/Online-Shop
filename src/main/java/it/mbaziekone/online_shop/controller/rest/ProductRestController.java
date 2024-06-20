ccac925e7b3ecce6cba526decf1e3d8153ced7d3
M'bazie Kone <mbazie89@gmail.com>
2024-06-21 01:14:29 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/controller/rest/ProductRestController.java
+++ src/main/java/it/mbaziekone/online_shop/controller/rest/ProductRestController.java
@@ -3,6 +3,7 @@ package it.mbaziekone.online_shop.controller.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import it.mbaziekone.online_shop.service.ProductService;
 
 @RestController
 @RequestMapping("api/products")
+@CrossOrigin(origins = "http:localhost:4200")
 public class ProductRestController {
 	
 	@Autowired
