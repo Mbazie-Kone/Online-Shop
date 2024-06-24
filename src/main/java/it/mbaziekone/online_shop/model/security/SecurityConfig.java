fe3c739c0cf526543178c11b7b4378fd1bcf3489
M'bazie Kone <mbazie89@gmail.com>
2024-06-24 03:42:38 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
+++ src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -12,7 +13,10 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		
+		http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authz -> authz
+					.requestMatchers("/api/auth/**"))
 		return null;
 		
 	}
