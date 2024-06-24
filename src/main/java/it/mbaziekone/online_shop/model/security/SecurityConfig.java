0a808cb54c521ecbca6f072c732cab6363ad7e0f
M'bazie Kone <mbazie89@gmail.com>
2024-06-24 03:49:52 +0200

    Updates

--- src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
+++ src/main/java/it/mbaziekone/online_shop/model/security/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.csrf.CsrfAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
@@ -13,12 +12,13 @@ public class SecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
+		return http
 			.csrf(csrf -> csrf.disable())
 			.authorizeHttpRequests(authz -> authz
-					.requestMatchers("/api/auth/**"))
-		return null;
-		
+					.requestMatchers("/api/auth/**").permitAll()
+					.anyRequest().authenticated()
+			)
+			.httpBasic(httpBasic -> httpBasic.realmName("Online Shop"))
+			.build();
 	}
-
 }
