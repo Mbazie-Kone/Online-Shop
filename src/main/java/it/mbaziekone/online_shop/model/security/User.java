bbd6fa1f2c65c51124b4c70019bcf3056fba483e
M'bazie Kone <mbazie89@gmail.com>
2024-06-24 04:07:27 +0200

    Updates

---
+++ src/main/java/it/mbaziekone/online_shop/model/security/User.java
@@ -0,0 +1,36 @@
+package it.mbaziekone.online_shop.model.security;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable, UserDetails {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
